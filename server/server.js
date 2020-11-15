ae1c1f678879c0cb525b6735a017bd7be874a6d1
zimprod23 <zimproduction23@gmail.com>
2020-11-15 19:36:22 +0100

    nubfiuew

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ const app = require("./app");
 const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    console.log("Production");
+  });
+}
+
 server.listen(PORT, () => {
   console.log("we are @ " + PORT);
 });
